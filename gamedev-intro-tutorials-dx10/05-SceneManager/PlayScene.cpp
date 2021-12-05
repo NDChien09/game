5521a3835de648a272ef0b2e2405a14d2f3fd4a7
NDChien09 <90445656+NDChien09@users.noreply.github.com>
2021-12-05 15:44:13 +0700

    Add all colorbox

--- gamedev-intro-tutorials-dx10/05-SceneManager/PlayScene.cpp
+++ gamedev-intro-tutorials-dx10/05-SceneManager/PlayScene.cpp
@@ -9,7 +9,6 @@
 #include "Portal.h"
 #include "Coin.h"
 #include "Platform.h"
-#include "NewBrick.h"
 #include "ColorBox.h"
 
 #include "SampleKeyEventHandler.h"
@@ -121,7 +120,6 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	case OBJECT_TYPE_GOOMBA: obj = new CGoomba(x,y); break;
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
-	case OBJECT_TYPE_NEWBRICK: obj = new CNewBrick(x, y); break;
 
 	case OBJECT_TYPE_PLATFORM:
 	{
