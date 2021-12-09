f09869771a6e7016ceb6f67650b8605ac6231e11
NDChien09 <90445656+NDChien09@users.noreply.github.com>
2021-12-09 09:48:28 +0700

    Add koopas, ani of koopas

--- gamedev-intro-tutorials-dx10/05-SceneManager/Mario.cpp
+++ gamedev-intro-tutorials-dx10/05-SceneManager/Mario.cpp
@@ -7,6 +7,7 @@
 #include "Goomba.h"
 #include "Coin.h"
 #include "Portal.h"
+#include "Koopas.h"
 
 #include "Collision.h"
 #include "ColorBox.h"
@@ -63,6 +64,8 @@ void CMario::OnCollisionWith(LPCOLLISIONEVENT e)
 
 	if (dynamic_cast<CGoomba*>(e->obj))
 		OnCollisionWithGoomba(e);
+	else if (dynamic_cast<CGoomba*>(e->obj))
+		OnCollisionWithKoopas(e);
 	else if (dynamic_cast<CCoin*>(e->obj))
 		OnCollisionWithCoin(e);
 	else if (dynamic_cast<CPortal*>(e->obj))
@@ -87,6 +90,39 @@ void CMario::OnCollisionWithGoomba(LPCOLLISIONEVENT e)
 		if (untouchable == 0)
 		{
 			if (goomba->GetState() != GOOMBA_STATE_DIE)
+			{
+				if (level > MARIO_LEVEL_SMALL)
+				{
+					level = MARIO_LEVEL_SMALL;
+					StartUntouchable();
+				}
+				else
+				{
+					DebugOut(L">>> Mario DIE >>> \n");
+					SetState(MARIO_STATE_DIE);
+				}
+			}
+		}
+	}
+
+}void CMario::OnCollisionWithKoopas(LPCOLLISIONEVENT e)
+{
+	CKoopas* koopas = dynamic_cast<CKoopas*>(e->obj);
+
+	// jump on top >> kill Koopas and deflect a bit 
+	if (e->ny < 0)
+	{
+		if (koopas->GetState() != KOOPAS_STATE_DIE)
+		{
+			koopas->SetState(KOOPAS_STATE_DIE);
+			vy = -MARIO_JUMP_DEFLECT_SPEED;
+		}
+	}
+	else // hit by Koopas
+	{
+		if (untouchable == 0)
+		{
+			if (koopas->GetState() != KOOPAS_STATE_DIE)
 			{
 				if (level > MARIO_LEVEL_SMALL)
 				{
